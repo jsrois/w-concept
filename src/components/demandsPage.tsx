76d29a131c07d1b27caf90f0f957910cdfad13be
Javier Sánchez <javier.sanchezrois@thoughtworks.com>
2020-04-23 16:55:13 +0200

    added sample text

--- src/components/demandsPage.tsx
+++ src/components/demandsPage.tsx
@@ -3,6 +3,9 @@ import * as React from "react";
 export const DemandsPage = (): JSX.Element =>
     <div className="demands-content hero-image">
         <div className="hero-text">
-            Here are the demands
+            <p className="hero-text__phrase first">stop all evictions</p>
+            <p className="hero-text__phrase second">cancel rent and debt</p>
+            <p className="hero-text__phrase third">housing for all, immediately</p>
+            <p className="hero-text__phrase fourth">establish rent controls</p>
         </div>
     </div>;
