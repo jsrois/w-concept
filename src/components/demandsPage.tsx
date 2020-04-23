77aa14e4b82a085d3bb47f6df5f05a3f079d4b77
Javier Sánchez <javier.sanchezrois@thoughtworks.com>
2020-04-23 16:20:30 +0200

    Improved basic layout

--- src/components/demandsPage.tsx
+++ src/components/demandsPage.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 
 export const DemandsPage = (): JSX.Element =>
-    <div className="content-demands">
-        <div className="background"></div>
-        <div className="overlay"></div>
-        <div className="text">this are the demands</div>
+    <div className="demands-content hero-image">
+        <div className="hero-text">
+            Here are the demands
+        </div>
     </div>;
