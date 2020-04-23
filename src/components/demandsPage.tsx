e74eb9b47566a127eb4a2f0f97d5d2787cc9696e
Javier Sánchez <javier.sanchezrois@thoughtworks.com>
2020-04-23 19:19:23 +0200

    Add tooltips

--- src/components/demandsPage.tsx
+++ src/components/demandsPage.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
+import ReactTooltip from "react-tooltip";
 
 export const DemandsPage = (): JSX.Element =>
     <div className="demands-content hero-image">
-        <div className="hero-text">
+        <div data-tip data-for="tt-demands" className="hero-text">
             <p className="hero-text__phrase first">stop all evictions</p>
             <p className="hero-text__phrase second">cancel rent and debt</p>
             <p className="hero-text__phrase third">housing for all, immediately</p>
             <p className="hero-text__phrase fourth">establish rent controls</p>
         </div>
+        <ReactTooltip id="tt-demands"> Here we will put our demands </ReactTooltip>
     </div>;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,15 @@
 import {Link} from "react-router-dom";
 import * as React from "react";
 import logo from '../images/logo.png';
+import ReactTooltip from "react-tooltip";
 
 export const Header = (): JSX.Element => (
     <header className="vertical-layout__header">
-        <img className="logo" src={logo} />
+        <img data-tip data-for="tt-logo" className="logo" src={logo} />
         <nav className="navigation-bar">
             <Link to="/">Our Demands</Link>
             <Link to="/organizations">The Network</Link>
         </nav>
+        <ReactTooltip id="tt-logo">Here we'll put our logo</ReactTooltip>
     </header>
 );
