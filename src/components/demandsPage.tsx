3979fd1111774b91953836ffc330bea12e00e6d9
Javier Sánchez <javier.sanchezrois@thoughtworks.com>
2020-04-25 15:59:28 +0200

    added logo and extra stylings for header bar

--- src/components/demandsPage.tsx
+++ src/components/demandsPage.tsx
@@ -2,12 +2,11 @@ import * as React from "react";
 import ReactTooltip from "react-tooltip";
 
 export const DemandsPage = (): JSX.Element =>
-    <div data-tip data-for="tt-background" className="demands-content hero-image">
-        <div data-tip data-for="tt-demands" className="hero-text">
+    <div className="demands-content hero-image">
+        <div className="hero-text">
             <p className="hero-text__phrase first">stop all evictions</p>
             <p className="hero-text__phrase second">cancel rent and debt</p>
             <p className="hero-text__phrase third">housing for all, immediately</p>
             <p className="hero-text__phrase fourth">free sandwiches and free beer🍻</p>
         </div>
-        <ReactTooltip id="tt-demands"> Here we could put our demands. The image is a placeholder.</ReactTooltip>
     </div>;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,15 @@
 import {Link} from "react-router-dom";
 import * as React from "react";
-import logo from '../images/logo.png';
+import logo from '../images/tsn-logo-centered-white.png';
 import ReactTooltip from "react-tooltip";
 
 export const Header = (): JSX.Element => (
     <header className="vertical-layout__header">
-        <img data-tip data-for="tt-logo" className="logo" src={logo} />
         <nav className="navigation-bar">
-            <Link data-tip data-for="tt-demands-button"  to="/">Our Demands</Link>
+            <div data-tip data-for="tt-logo" className="logo-box">
+                <img data-tip data-for="tt-logo" className="logo" src={logo}/>
+            </div>
+            <Link data-tip data-for="tt-demands-button" to="/">Our Demands</Link>
             <Link data-tip data-for="tt-campaign-link" to="/">#OurCoolHashtag</Link>
             <Link data-tip data-for="tt-orgs" to="/organizations">The Network</Link>
         </nav>
