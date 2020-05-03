56eca37b47e662c86eb55cc1c7bdb42f267bef7e
Javier Sánchez <javier.sanchezrois@thoughtworks.com>
2020-05-03 19:08:31 +0200

    Rearrange pages and tabs

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+
+export const MainPage = (): JSX.Element => (<div className="mainpage-content"></div>)
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,16 +6,11 @@ import ReactTooltip from "react-tooltip";
 export const Header = (): JSX.Element => (
     <header className="vertical-layout__header">
         <nav className="navigation-bar">
-            <div data-tip data-for="tt-logo" className="logo-box">
-                <img data-tip data-for="tt-logo" className="logo" src={logo}/>
+            <div className="logo-box">
+                <img className="logo" src={logo}/>
             </div>
-            <Link data-tip data-for="tt-demands-button" to="/">Our Demands</Link>
-            <Link data-tip data-for="tt-campaign-link" to="/">#OurCoolHashtag</Link>
-            <Link data-tip data-for="tt-orgs" to="/organizations">The Network</Link>
+            <Link  to="/covid19-demands">Covid-19 Crisis</Link>
+            <Link to="/organizations">The Network</Link>
         </nav>
-        <ReactTooltip id="tt-logo">Here we could put our logo</ReactTooltip>
-        <ReactTooltip id="tt-orgs">Information about the organizations in this network</ReactTooltip>
-        <ReactTooltip id="tt-demands-button">Here we could put our demands</ReactTooltip>
-        <ReactTooltip id="tt-campaign-link">Here we could put the info about the campaign</ReactTooltip>
     </header>
 );
