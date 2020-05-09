98da6d9e15b426af1132da9d38ff2a5b5cef6cf1
Javier Sánchez <javier.sanchezrois@thoughtworks.com>
2020-05-09 20:49:19 +0200

    Added full demands text

--- src/components/demandsPage.tsx
+++ src/components/demandsPage.tsx
@@ -1,11 +1,33 @@
 import * as React from "react";
-import ReactTooltip from "react-tooltip";
 
 export const DemandsPage = (): JSX.Element =>
     <div className="demands-content hero-image">
         <div className="hero-text">
-            <p className="hero-text__phrase first">stop all evictions</p>
-            <p className="hero-text__phrase second">cancel rent and debt</p>
-            <p className="hero-text__phrase third">housing for all, immediately</p>
+            <div className="second hero-text__phrase">
+                <p className="title">Adequate and safe housing for all</p>
+                <div className="description">
+                    <p>All housing must be a public healthcare concern.</p>
+                    <p>House the most vulnerable: the homeless, domestic violence survivors, undocumented migrants,
+                        refugee and asylum seekers.</p>
+                </div>
+            </div>
+            <div className="first hero-text__phrase">
+                <p className="title">#CancelRent</p>
+                <p className="description">
+                    <p>Rent, mortgages and housing payments should be cancelled for the duration of the pandemic
+                        crisis.</p>
+                    <p>All arrears accumulated during the crisis should be written off.</p>
+                </p>
+            </div>
+            <div className="third hero-text__phrase">
+                <p className="title">Homes for people, not for profit</p>
+                <div className="description">
+                    <p>Expropriation of empty or misused private housing, including short term holiday rentals.</p>
+                    <p>Public housing policies should enforce housing as a right over its mercantilization in the long
+                        term.</p>
+                </div>
+            </div>
         </div>
     </div>;
+
+
