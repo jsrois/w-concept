96274d7b90cd1375e0844c1eb8590a7592f67526
Javier Sánchez <javier.sanchezrois@thoughtworks.com>
2020-05-10 00:43:03 +0200

    Refactor demands into components

--- src/components/demandsPage.tsx
+++ src/components/demandsPage.tsx
@@ -1,33 +1,36 @@
 import * as React from "react";
+import {useState} from "react";
 
-export const DemandsPage = (): JSX.Element =>
-    <div className="demands-content hero-image">
-        <div className="hero-text">
-            <div className="second hero-text__phrase hero-text__phrase--collapse">
-                <p className="title">Adequate and safe housing for all</p>
-                <div className="description">
-                    <p>All housing must be a public healthcare concern.</p>
-                    <p>House the most vulnerable: the homeless, domestic violence survivors, undocumented migrants,
-                        refugee and asylum seekers.</p>
-                </div>
-            </div>
-            <div className="first hero-text__phrase hero-text__phrase--collapse">
-                <p className="title">#CancelRent</p>
-                <p className="description">
-                    <p>Rent, mortgages and housing payments should be cancelled for the duration of the pandemic
-                        crisis.</p>
-                    <p>All arrears accumulated during the crisis should be written off.</p>
-                </p>
-            </div>
-            <div className="third hero-text__phrase hero-text__phrase--collapse">
-                <p className="title">Homes for people, not for profit</p>
-                <div className="description">
-                    <p>Expropriation of empty or misused private housing, including short term holiday rentals.</p>
-                    <p>Public housing policies should enforce housing as a right over its mercantilization in the long
-                        term.</p>
-                </div>
-            </div>
+
+const CollapsableDemand = (props: { title: string, description: string[] }) => {
+    return <div className="hero-text__phrase hero-text__phrase--collapse">
+        <p className="title">{props.title}</p>
+        <div className="description">
+            {props.description.map(p => <p>{p}</p>)}
         </div>
     </div>;
+}
+
+export const DemandsPage = (): JSX.Element => {
+
+    const [selected, setSelected] = useState("");
+
+    return <div className="demands-content hero-image">
+        <div className="hero-text">
+            <CollapsableDemand title="Adequate and safe housing for all" description={[
+                "All housing must be a public healthcare concern.",
+                "House the most vulnerable: the homeless, domestic violence survivors, undocumented migrants, refugee and asylum seekers."
+            ]}/>
+            <CollapsableDemand title={"#CancelRent"} description={[
+                "Rent, mortgages and housing payments should be cancelled for the duration of the pandemic crisis.",
+                "All arrears accumulated during the crisis should be written off."
+            ]}/>
+            <CollapsableDemand title={"Homes for people, not for profit"} description={[
+                "Expropriation of empty or misused private housing, including short term holiday rentals.",
+                "Public housing policies should enforce housing as a right over its mercantilization in the long term."
+            ]}/>
+        </div>
+    </div>;
+};
 
 
