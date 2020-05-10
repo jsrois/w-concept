1e88755d796ffc59bcd331effed6b497ef565766
Javier Sánchez <javier.sanchezrois@thoughtworks.com>
2020-05-10 10:38:31 +0200

    refactored demands content and style without animation

--- src/components/demandsPage.tsx
+++ src/components/demandsPage.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 import {useState} from "react";
+import * as Data from "../data/demands.json";
 
+interface DemandsInfo {
+    title: string;
+    description: string[];
+}
 
-const CollapsableDemand = (props: { title: string, description: string[] }) => {
-    return <div className="hero-text__phrase hero-text__phrase--collapse">
+const demands = Data.demands as DemandsInfo[];
+
+const CollapsableDemand = (props: { key: string, title: string, description: string[], onSelection: () => void, selected: boolean }) => {
+    return <div className={props.selected ? "hero-text__phrase hero-text__phrase--selected" : "hero-text__phrase"}
+                onMouseUp={props.onSelection}>
         <p className="title">{props.title}</p>
         <div className="description">
             {props.description.map(p => <p>{p}</p>)}
@@ -11,24 +19,20 @@ const CollapsableDemand = (props: { title: string, description: string[] }) => {
     </div>;
 }
 
+
 export const DemandsPage = (): JSX.Element => {
 
-    const [selected, setSelected] = useState("");
+    const [selectedKey, setSelectedKey] = useState("");
 
     return <div className="demands-content hero-image">
         <div className="hero-text">
-            <CollapsableDemand title="Adequate and safe housing for all" description={[
-                "All housing must be a public healthcare concern.",
-                "House the most vulnerable: the homeless, domestic violence survivors, undocumented migrants, refugee and asylum seekers."
-            ]}/>
-            <CollapsableDemand title={"#CancelRent"} description={[
-                "Rent, mortgages and housing payments should be cancelled for the duration of the pandemic crisis.",
-                "All arrears accumulated during the crisis should be written off."
-            ]}/>
-            <CollapsableDemand title={"Homes for people, not for profit"} description={[
-                "Expropriation of empty or misused private housing, including short term holiday rentals.",
-                "Public housing policies should enforce housing as a right over its mercantilization in the long term."
-            ]}/>
+            {
+                demands.map((d, k) =>
+                    <CollapsableDemand {...d}
+                                       key={k.toString()}
+                                       onSelection={() => setSelectedKey(k.toString())}
+                                       selected={selectedKey === k.toString()}/>)
+            }
         </div>
     </div>;
 };
