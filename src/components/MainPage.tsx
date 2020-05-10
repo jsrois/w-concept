3efbd9d4d1caa0311e2bf5ad3a504b3a3e6f484c
Javier Sánchez <javier.sanchezrois@thoughtworks.com>
2020-05-10 19:59:55 +0200

    Added picture for main page

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,3 +1,8 @@
 import * as React from 'react';
 
-export const MainPage = (): JSX.Element => (<div className="mainpage-content"></div>)
+export const MainPage = (): JSX.Element => (
+    <div className="mainpage-content hero-image">
+        <div className="hero-text">
+            We are an international <strong>loose network</strong> of tenant unions and local organizations in defense of housing.
+        </div>
+    </div>);
