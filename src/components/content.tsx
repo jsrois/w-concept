1d1992622441087ad30ace09e75cda8d9e9ed940
Javier Sánchez <javier.sanchezrois@thoughtworks.com>
2020-05-12 22:38:08 +0200

    Single page

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
-import {Route, Switch} from "react-router-dom";
 import {DemandsPage} from "./demandsPage";
 import {OrganizationsPage} from "./organizationsPage";
 import {MainPage} from "./MainPage";
 
 export const Content = (): JSX.Element => (
     <div className="vertical-layout__content">
-        <Switch>
-            <Route exact path="/" component={MainPage}/>
-            <Route path="/covid19-demands" component={DemandsPage}/>
-            <Route path="/organizations" component={OrganizationsPage}/>
-        </Switch>
+       <MainPage />
+       <DemandsPage />
+       <OrganizationsPage />
     </div>
 );
